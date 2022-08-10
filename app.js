666011cd2ad540b9c0072d0013f2edcd3b720343
Habeeb <hbankole06@gmail.com>
2022-08-10 19:13:42 +0100

    add endpoint for fetching a single parcel

--- app.js
+++ app.js
@@ -10,26 +10,26 @@ app.use(cors())
 
 const parcels =[
     {
-        Id: 1,
+        id: 1,
         product: "Washing machine",
         description: "LG Washing Machine",
         deliveryDate: "10/08/2022",
     },
     {
-        Id: 2,
+        id: 2,
         product: "Sewing Machine",
         description: "Two lion sewing machine domestic",
         deliveryDate: "08/08/2022",
 
     },
     {
-        Id: 3,
+        id: 3,
         product: "steam iron",
         description: "Philips steam iron",
         deliveryDate: "01/07/2022",
     },
     {
-        Id: 4,
+        id: 4,
         product: "Air conditioner",
         description: "TCL 1.5HP Air conditioner",
         deliveryDate: "02/06/2022"
@@ -41,6 +41,18 @@ app.get('/parcels', (req, res) => {
         data: parcels
         
     })
+})
+
+app.get('/parcels/:id' , (req, res) => {
+    const { id } = req.params;
+    console.log('Id ', id)
+    let found = parcels.find((parcel) => parcel.id === parseInt(id) )
+    if (found){
+        res.status(200).json({data: found})
+    }
+    else{
+        res.sendStatus(400)
+    }
 })
 
 app.listen(PORT, () => {
