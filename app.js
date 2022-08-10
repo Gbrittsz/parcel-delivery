5cbaf0637c7d0e330c71bc3c2a3676c54c2f9df1
Habeeb <hbankole06@gmail.com>
2022-08-10 07:53:13 +0100

    add cors middleware

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require("cors")
+
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.json({
